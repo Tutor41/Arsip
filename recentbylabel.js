390fe61ad8718771843a9dae3589e34a1c84eb53
Tutor41 <41tutor@gmail.com>
2018-07-28 04:11:49 +0700

    Update recentbylabel.js

--- recentbylabel.js
+++ recentbylabel.js
@@ -1,1 +1,48 @@
-function rcentbytag(t){document.write('<ul class="recent-by-tag">');for(var e=0;e<numposts;e++){var r,n,i=t.feed.entry[e],l=i.title.$t;if(e==t.feed.entry.length)break;for(var o=0;o<i.link.length;o++){if("replies"==i.link[o].rel&&"text/html"==i.link[o].type)i.link[o].title,i.link[o].href;if("alternate"==i.link[o].rel){r=i.link[o].href;break}}try{n=i.media$thumbnail.url}catch(t){s=i.content.$t,a=s.indexOf("<img"),b=s.indexOf('src="',a),c=s.indexOf('"',b+5),d=s.substr(b+5,c-b-5),n=-1!=a&&-1!=b&&-1!=c&&""!=d?d:"http://2.bp.blogspot.com/-giova1ZCh-A/Uzq6L8QTJNI/AAAAAAAAAJc/USXictTq_xs/s70-c/KM+Icon.png"}var u=i.published.$t,m=u.substring(0,4),g=u.substring(5,7),h=u.substring(8,10),f=new Array;f[1]="Januari",f[2]="Februari",f[3]="Maret",f[4]="April",f[5]="Mei",f[6]="Juni",f[7]="Juli",f[8]="Agustus",f[9]="September",f[10]="Oktober",f[11]="November",f[12]="Desember",document.write('<li class="clear">'),1==showpostthumbnails&&document.write('<a href="'+r+'" target ="_blank" title="'+l+'"><img class="rct-thumb" alt="'+l+'" src="'+n+'"/></a>'),document.write('<strong><a href="'+r+'" target ="_blank" title="'+l+'" rel="nofollow">'+l+"</a></strong>"),document.write("<br>");var p="";1==showpostdate&&(p='<span class="showdates">'+p+h+" "+f[parseInt(g,10)]+" "+m+"</span>",1),document.write(p),document.write("</li>"),e!=numposts-1&&document.write("")}document.write("</ul>")}
+$('.ready-widget .HTML .widget-content span.mainlabel').each(function () {
+	var v = $(this).attr("data-label"),
+	b = $(this).attr("data-no");
+	$.ajax({
+		url: "/feeds/posts/default/-/" + v + "?alt=json-in-script&max-results=" + b,
+		type: 'get',
+		dataType: "jsonp",
+		success: function (e) {
+			var u = "";
+			var h = '<ul class="custom-widget">';
+			for (var i = 0; i < e.feed.entry.length; i++) {
+				for (var j = 0; j < e.feed.entry[i].link.length; j++) {
+					if (e.feed.entry[i].link[j].rel == "alternate") {
+						u = e.feed.entry[i].link[j].href;
+						break
+					}
+				}
+				var g = e.feed.entry[i].title.$t;
+				var s = e.feed.entry[i].category[0].term;
+				var y = e.feed.entry[i].author[0].name.$t;
+				var d = e.feed.entry[i].published.$t,
+				t = d.substring(0, 4),
+				w = d.substring(5, 7),
+				f = d.substring(8, 10),
+				r = month_format[parseInt(w, 10)] + ' ' + f + ', ' + t;
+				var c = e.feed.entry[i].content.$t;
+				var $c = $('<div>').html(c);
+				if (c.indexOf("//www.youtube.com/embed/") > -1) {
+					var p = e.feed.entry[i].media$thumbnail.url.replace('/default.jpg', '/mqdefault.jpg');
+					var k = p
+				} else if (c.indexOf("<img") > -1) {
+					var q = $c.find('img:first').attr('src').replace('s72-c', 's1600');
+					var k = q
+				} else {
+					var k = no_image
+				}
+				h += '<li><a class="rcthumb" href="' + u + '" style="background:url(' + k + ') no-repeat center center;background-size: cover"><span class="img-overlay"/></a><div class="post-panel"><h3 class="rcp-title"><a href="' + u + '">' + g + '</a></h3><span class="recent-author">' + y + '</span><span class="recent-date">' + r + '</span></div></li>'
+			}
+			h += '</ul>';
+			$(".ready-widget .HTML .widget-content span.mainlabel").each(function () {
+				var text = $(this).attr("data-label");
+				if (text == v) {
+					$(this).parent().html(h)
+				}
+			})
+		}
+	})
+});
